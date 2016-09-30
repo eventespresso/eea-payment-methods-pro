975ab597a43f7f43dbe2d73cd8ea455d4d7a6b73
Brent Christensen <brent@eventespresso.com>
2016-09-30 13:07:11 -0700

    fix php docs, typos, and formatting; remove unused var; [touch:9433]

--- core/db_class_extensions/EEE_Payment_Methods_Pro_Payment_Method.class_ext.php
+++ core/db_class_extensions/EEE_Payment_Methods_Pro_Payment_Method.class_ext.php
@@ -15,22 +15,31 @@ if (!defined('EVENT_ESPRESSO_VERSION')) {
  */
 class EEE_Payment_Methods_Pro_Payment_Method extends EEE_Base_Class{
 
-	public function __construct(){
+    /**
+     * EEE_Payment_Methods_Pro_Payment_Method constructor.
+     *
+     * @throws EE_Error
+     */
+    public function __construct(){
 		$this->_model_name_extended = 'Payment_Method';
 		parent::__construct();
 	}
 
-	/**
-	 * Sets these payment method IDs to be the only paymen tmethods related to this event
-	 * (ie overwrites previous relations)
-	 * @param boolean $on_by_default IDs of related payment methods
-	 * @return boolean
-	 */
+
+
+    /**
+     * Sets these payment method IDs to be the only payment methods related to this event
+     * (ie overwrites previous relations)
+     *
+     * @param boolean $on_by_default IDs of related payment methods
+     * @return void
+     * @throws EE_Error
+     */
 	public function ext_set_available_by_default( $on_by_default ){
-		//if this payment method is being set to being on by default, 
+		//if this payment method is being set to being on by default,
 		//we need to ensure there are no other payment methods are on by default
 		//(we think folks will only want one payment method of a given type
-		//active on an event at a time; but they're allowed to have lots of 
+		//active on an event at a time; but they're allowed to have lots of
 		//inactive ones of course)
 		//get the previous value
 		$previous_default_availability = $this->_->is_available_by_default();
@@ -53,7 +62,7 @@ class EEE_Payment_Methods_Pro_Payment_Method extends EEE_Base_Class{
 				//update its extra meta
 				$previous_default_payment_method->update_extra_meta( EED_Payment_Methods_Pro_More_Payment_Methods::on_by_default_meta_key, false );
 				//and remove any exceptions for it
-				$deletions = EEM_Extra_Join::instance()->delete( 
+				EEM_Extra_Join::instance()->delete(
 					array(
 						array(
 							'EXJ_first_model_name' => 'Event',
@@ -66,16 +75,20 @@ class EEE_Payment_Methods_Pro_Payment_Method extends EEE_Base_Class{
 		}
 		//have they changed the payment method's default availability?
 		//if so, they probably want it to take effect now, so remove all the exceptions
-		if( $previous_default_availability != $on_by_default ) {
+		if( $previous_default_availability !== $on_by_default ) {
 			$this->_->remove_availability_exceptions();
 		}
 		$this->_->update_extra_meta( EED_Payment_Methods_Pro_More_Payment_Methods::on_by_default_meta_key, $on_by_default );
 	}
-	
-	/**
-	 * Whether or not this payment method is available by default on all events
-	 * @return boolean
-	 */
+
+
+
+    /**
+     * Whether or not this payment method is available by default on all events
+     *
+     * @return bool
+     * @throws EE_Error
+     */
 	public function ext_is_available_by_default(){
 		$string_val = $this->_->get_extra_meta( EED_Payment_Methods_Pro_More_Payment_Methods::on_by_default_meta_key, true, '1' );
 		if( $string_val === '1' ) {
@@ -84,15 +97,19 @@ class EEE_Payment_Methods_Pro_Payment_Method extends EEE_Base_Class{
 			return false;
 		}
 	}
-	
-	/**
-	 * Removes all the payment methods' availability exceptions. So if this payme
-	 * method should be available on all events by default, it will be. Or if
-	 * it should NOT be available on events by default, then it won't be.
-	 * @return int the number of availability exceptions that were deleted
-	 */
+
+
+
+    /**
+     * Removes all the payment methods' availability exceptions. So if this payment
+     * method should be available on all events by default, it will be. Or if
+     * it should NOT be available on events by default, then it won't be.
+     *
+     * @return int the number of availability exceptions that were deleted
+     * @throws EE_Error
+     */
 	public function ext_remove_availability_exceptions() {
-		return EEM_Extra_Join::instance()->delete( 
+		return EEM_Extra_Join::instance()->delete(
 			array(
 				array(
 					'EXJ_first_model_name' => 'Event',
